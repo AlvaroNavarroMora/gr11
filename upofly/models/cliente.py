d741fb525eeeb05d493a076665f1a68aeef428df
markendos <markendos@gmail.com>
2019-11-22 19:47:04 +0100

    Search view con filtro para clientes

--- upofly/models/cliente.py
+++ upofly/models/cliente.py
@@ -5,6 +5,5 @@ from odoo import models, fields, api
 class Cliente(models.Model):
     _name = 'upofly.cliente'
     _inherits = {'res.partner' : 'partner_id'}
-    partner_id = fields.Many2one('res.partner', ondelete='cascade'
-                                 )
+    partner_id = fields.Many2one('res.partner', ondelete='cascade')
     identificacion = fields.Char('DNI/CIF', size=9)

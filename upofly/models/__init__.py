ade8a6a36204158e8ab12851a3e2271c2a29706d
AmalioF96 <amalicabeza.16@gmail.com>
2019-11-24 22:10:35 +0100

    Vuelo y _rec_name --> Piloto y Ruta

--- upofly/models/__init__.py
+++ upofly/models/__init__.py
@@ -6,8 +6,5 @@ from . import parteMantenimiento
 from . import ruta
 from . import cliente
 from . import piloto
-<<<<<<< HEAD
 from . import factura
 from . import vuelo
-=======
->>>>>>> branch 'master' of https://github.com/markendos/gr11.git
--- upofly/models/ruta.py
+++ upofly/models/ruta.py
@@ -5,6 +5,7 @@ from odoo import models, fields, api
 
 class ruta(models.Model):
     _name = 'upofly.ruta'
+    _rec_name = 'nombre'
     
     nombre = fields.Char('Nombre', size=64, required=True)
     descripcion = fields.Char("Descripcion", size=64,required=True)

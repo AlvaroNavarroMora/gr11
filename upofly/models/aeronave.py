0840cafe00954a814ec2b633e2fadf00317a0dfd
markendos <markendos@gmail.com>
2019-11-24 22:54:57 +0100

    Vista search con filtros para Partes de Mantenimiento

--- upofly/models/aeronave.py
+++ upofly/models/aeronave.py
@@ -6,8 +6,8 @@ class aeronave(models.Model):
     _name = 'upofly.aeronave'
     _rec_name = 'matricula'
     
-    modelo = fields.Char('Nombre modelo', size=64, required=True)
+    modelo = fields.Char('Modelo', size=64, required=True)
     capacidad = fields.Integer("Capacidad", required=True)
     suplemento = fields.Float("Suplemento")
-    matricula = fields.Char("Matricula aeronave", size=6, required=True)
+    matricula = fields.Char("Matrícula", size=6, required=True)
     parte_mantenimiento_ids = fields.One2many("upofly.parte_mantenimiento", "aeronave_id", string="Partes de Mantenimiento")

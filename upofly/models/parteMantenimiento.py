0572d0144022f09591888bdf809047c47799d106
AlvaroNavarroMora <alvaronavarromroa@gmail.com>
2019-11-24 14:22:07 +0100

    Actualizacion de aeronave y parteMantenimiento

--- upofly/models/parteMantenimiento.py
+++ upofly/models/parteMantenimiento.py
@@ -4,9 +4,10 @@ from odoo import models, fields, api
 
 class parteMantenimiento(models.Model):
     _name = 'upofly.parte_mantenimiento'
+    _rec_name = 'estado'
     
     fecha = fields.Date("Fecha", required=True)
     descripcion = fields.Text("Descripcion")
-    estado = fields.Selection([('enEspera', 'En espera'), ('enReparacion', 'En reparacion'), ('reparado', 'Reparado')], 'Estado del mantenimiento')
+    estado = fields.Selection([('En espera', 'En espera'), ('En reparacion', 'En reparacion'), ('Reparado', 'Reparado')], 'Estado del mantenimiento')
     tiempoEstimado = fields.Integer("Tiempo Estimado de reparación (Días)", required = True)
     aeronave_id = fields.Many2one("upofly.aeronave", "Aeronave")

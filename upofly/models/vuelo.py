56e5b36f0b5d8f17d6b4378c5109e6a0c5c6fd4a
AmalioF96 <amalicabeza.16@gmail.com>
2019-11-30 17:41:24 +0100

    Vista de calendar semanal para el modelo Vuelo

--- upofly/models/vuelo.py
+++ upofly/models/vuelo.py
@@ -6,7 +6,7 @@ from odoo import models, fields, api
 class vuelo(models.Model):
     _name = 'upofly.vuelo'
     
-    fecha = fields.Date('Fecha', size=64, required=True)
+    fecha = fields.Date('Fecha', required=True)
     aeronave_id = fields.Many2one("upofly.aeronave", "Aeronave")
     ruta_id = fields.Many2one("upofly.ruta", "Ruta")
     piloto_id = fields.Many2one("upofly.piloto", "Piloto")

06e017b46fc6a4abe9658b3112c9493a7c181e6d
AlvaroNavarroMora <alvaronavarromora@gmail.com>
2019-12-01 17:27:45 +0100

    Quitar Suplemento

--- upofly/models/aeronave.py
+++ upofly/models/aeronave.py
@@ -8,6 +8,5 @@ class aeronave(models.Model):
     
     modelo = fields.Char('Nombre modelo', size=64, required=True)
     capacidad = fields.Integer("Capacidad", required=True)
-    suplemento = fields.Float("Suplemento")
     matricula = fields.Char("Matricula aeronave", size=6, required=True)
     parte_mantenimiento_ids = fields.One2many("upofly.parte_mantenimiento", "aeronave_id", string="Partes de Mantenimiento")

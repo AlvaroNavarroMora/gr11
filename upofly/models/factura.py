325d1310bffae13f7bf03887e5105ef4244de9a8
AlvaroNavarroMora <alvaronavarromora@gmail.com>
2019-12-28 14:29:36 +0100

    Actualización Factura

--- upofly/models/factura.py
+++ upofly/models/factura.py
@@ -7,14 +7,13 @@ class factura(models.Model):
      _rec_name = 'identificador'
 
      identificador = fields.Integer("ID", required=True)
-     iva = fields.Float("IVA%", required=True)
-     subtotal = fields.Float("Subtotal", required=True)
-     importeTotal = fields.Float("Importe total", compute="_importe_total")
+     iva = fields.Float("IVA%", required=True)     
      concepto = fields.Char("Concepto", required=True)
      descripcion = fields.Text("Descripcion")
      fecha = fields.Date("Fecha")
      venta_id = fields.Many2one("upofly.venta", "Venta")
-
+     subtotal = fields.Float("Subtotal", related='venta_id.total')
+     importeTotal = fields.Float("Importe total", compute="_importe_total")
          
      @api.depends('subtotal', 'iva')
      def _importe_total(self):

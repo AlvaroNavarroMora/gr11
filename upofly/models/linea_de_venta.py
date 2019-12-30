719a95e3df752cc1eb1e90237028666f4461bcea
AlvaroNavarroMora <alvaronavarromora@gmail.com>
2019-12-30 21:02:37 +0100

    Añadir relacion linea_de_venta - vuelo

--- upofly/models/linea_de_venta.py
+++ upofly/models/linea_de_venta.py
@@ -4,9 +4,20 @@ from odoo import models, fields, api
 
 class linea_de_venta(models.Model):
     _name = 'upofly.linea_de_venta'
-    _rec_name = 'precio'
+    _rec_name = 'num_horas'
     
-    precio = fields.Float("Precio")
+    num_horas = fields.Float("Número de horas")
     venta_id = fields.Many2one("upofly.venta", "Venta")
     servicio_ids = fields.Many2one("upofly.servicio", "Servicios")
+    vuelo_id = fields.Many2one("upofly.vuelo", "Vuelo")
+    
+    
+    subtotal = fields.Float("Subtotal", compute="_calcular_subtotal")
+    precio_base_id = fields.Float("Precio Base", related='servicio_ids.precioBase')
+    precio_hora_id = fields.Float("Precio por Hora", related='servicio_ids.precioHora')
+    
+    @api.depends('num_horas', 'precio_base_id', 'precio_hora_id')
+    def _calcular_subtotal(self):
+        self.subtotal = float(self.precio_base_id) + (float(self.precio_hora_id) * float(self.num_horas))
+    
 
--- upofly/models/vuelo.py
+++ upofly/models/vuelo.py
@@ -5,8 +5,10 @@ from odoo import models, fields, api
 
 class vuelo(models.Model):
     _name = 'upofly.vuelo'
+    _rec_name = 'fecha'
     
     fecha = fields.Date('Fecha', required=True)
     aeronave_id = fields.Many2one("upofly.aeronave", "Aeronave")
     ruta_id = fields.Many2one("upofly.ruta", "Ruta")
     piloto_id = fields.Many2many("upofly.piloto", string="Piloto")
+    linea_de_venta_id = fields.Many2one('upofly.linea_de_venta', "Líneas de venta")

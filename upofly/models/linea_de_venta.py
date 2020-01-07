2bb9057a0df7690996b9f36b7bc8b320eb2b3713
AlvaroNavarroMora <alvaronavarromora@gmail.com>
2020-01-07 22:44:02 +0100

    Cálculo del precio total en el módulo venta

--- upofly/models/linea_de_venta.py
+++ upofly/models/linea_de_venta.py
@@ -12,12 +12,13 @@ class linea_de_venta(models.Model):
     vuelo_id = fields.Many2one("upofly.vuelo", "Vuelo")
     
     
-    subtotal = fields.Float("Subtotal", compute="_calcular_subtotal")
+    subtotal = fields.Float("Subtotal", store=True, compute="_calcular_subtotal")
     precio_base_id = fields.Float("Precio Base", related='servicio_ids.precio_base')
     precio_hora_id = fields.Float("Precio por Hora", related='servicio_ids.precio_hora')
     
     @api.depends('num_horas', 'precio_base_id', 'precio_hora_id')
     def _calcular_subtotal(self):
-        self.subtotal = float(self.precio_base_id) + (float(self.precio_hora_id) * float(self.num_horas))
+        for record in self:
+            record.subtotal = float(record.precio_base_id) + (float(record.precio_hora_id) * float(record.num_horas))
     
 
--- upofly/models/venta.py
+++ upofly/models/venta.py
@@ -6,14 +6,33 @@ class venta(models.Model):
     _name = 'upofly.venta'
     _rec_name = 'total'
     
-    total = fields.Float("Total")
+    #total = fields.Float("Total")
     factura_id = fields.Many2one("upofly.factura", "Factura")
     cliente_id = fields.Many2one("upofly.cliente", "Cliente")
     lineas_de_venta_ids = fields.One2many("upofly.linea_de_venta", "venta_id", string="Líneas de Venta")
     
-    #total = fields.Float("Total sin IVA", compute="_calcular_precio")
+    total = fields.Float("Total sin IVA", compute="_calcular_precio")
 
-    
-    #@api.depends()
-    #def _calcular_precio(self):
-        #self.total = sumar los subtotales de las lineass de venta
+    #@api.multi
+    #@api.depends('lineas_de_venta_ids', 'lineas_de_venta_ids.subtotal')
+    def _calcular_precio(self):
+        
+        '''tot = 0
+        for record in self:
+            for ldv in record.lineas_de_venta_ids:
+                
+                tot += sum(ven.subtotal for ven in ldv)
+            
+            record.update({'total' : tot})'''
+
+        
+        tot = 0
+        for record in self:
+            if record.lineas_de_venta_ids:
+                for ldv in record.lineas_de_venta_ids:
+                    
+                    for ven in ldv:
+                        if ven:
+                            tot = tot + ven.subtotal
+                #record.total = tot
+                record.update({'total' : tot})

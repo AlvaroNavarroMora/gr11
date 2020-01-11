c2d3b374c73e74cdb2b09a7617396fdde1d49fdf
Amalio <usuario@Odoo11>
2020-01-11 13:05:04 +0100

    Creacion de los ficheros de prueba. Demo1 -> Aeronaves, Localizaciones y Rutas Demo2 -> Clientes y Pilotos

--- upofly/__manifest__.py
+++ upofly/__manifest__.py
@@ -8,7 +8,7 @@
     'category': 'UPOFly',
     'version': '0.1',
     'depends': ['base'],
-    'demo': [],
+    'demo': ['demo/demo1.xml','demo/demo2.xml'],
     'data': ['views/aeronave_view.xml','views/parteMantenimiento_view.xml','views/ruta_view.xml','views/cliente_view.xml','views/piloto_view.xml','views/vuelo_view.xml','views/localizacion_view.xml','views/venta_view.xml','views/factura_view.xml','views/linea_de_venta_view.xml','views/servicio_view.xml'],
     'aplication': True,
 }

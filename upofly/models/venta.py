e8b53fb8a4f109218816adcc8c54717b78063204
AlvaroNavarroMora <alvaronavarromora@gmail.com>
2020-01-11 14:49:36 +0100

    Vista Graph en Venta

--- upofly/models/venta.py
+++ upofly/models/venta.py
@@ -6,25 +6,17 @@ class venta(models.Model):
     _name = 'upofly.venta'
     _rec_name = 'total'
     
-    #total = fields.Float("Total")
     factura_id = fields.Many2one("upofly.factura", "Factura")
     cliente_id = fields.Many2one("upofly.cliente", "Cliente")
     lineas_de_venta_ids = fields.One2many("upofly.linea_de_venta", "venta_id", string="Líneas de Venta")
-    
     total = fields.Float("Total sin IVA", compute="_calcular_precio")
-
+    total_aux = fields.Float("Preco total", compute="_calcular_precio_aux", store=True)
+    #total_aux = fields.Float("Precio total", store=true, compute="_calcular_precio2")
+    
+    
     #@api.multi
     #@api.depends('lineas_de_venta_ids', 'lineas_de_venta_ids.subtotal')
     def _calcular_precio(self):
-        
-        '''tot = 0
-        for record in self:
-            for ldv in record.lineas_de_venta_ids:
-                
-                tot += sum(ven.subtotal for ven in ldv)
-            
-            record.update({'total' : tot})'''
-
         
         tot = 0
         for record in self:
@@ -34,5 +26,12 @@ class venta(models.Model):
                     for ven in ldv:
                         if ven:
                             tot = tot + ven.subtotal
-                #record.total = tot
                 record.update({'total' : tot})
+    
+     #@api.depends('total')
+    def _calcular_precio_aux(self):
+        for record in self:
+            record.total2 = record.total
+    
+    
+    

2312696251b53d53a9cbc160f0ace2dcf7bb8979
AlvaroNavarroMora <alvaronavarromora@gmail.com>
2020-01-11 16:59:09 +0100

    Corrección venta

--- upofly/models/venta.py
+++ upofly/models/venta.py
@@ -11,27 +11,21 @@ class venta(models.Model):
     lineas_de_venta_ids = fields.One2many("upofly.linea_de_venta", "venta_id", string="Líneas de Venta")
     total = fields.Float("Total sin IVA", compute="_calcular_precio")
     total_aux = fields.Float("Preco total", compute="_calcular_precio_aux", store=True)
-    #total_aux = fields.Float("Precio total", store=true, compute="_calcular_precio2")
+
     
     
     #@api.multi
     #@api.depends('lineas_de_venta_ids', 'lineas_de_venta_ids.subtotal')
     def _calcular_precio(self):
-        
         tot = 0
         for record in self:
             if record.lineas_de_venta_ids:
-                for ldv in record.lineas_de_venta_ids:
-                    
-                    for ven in ldv:
-                        if ven:
-                            tot = tot + ven.subtotal
-                record.update({'total' : tot})
+                record.total = sum(ldv.subtotal for ldv in record.lineas_de_venta_ids)
+
     
      #@api.depends('total')
     def _calcular_precio_aux(self):
         for record in self:
             record.total2 = record.total
-    
     
     
